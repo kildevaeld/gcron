36604ef8f00ff59a68ad4b754d875ee1c86284cb
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2016-11-12 14:01:01 +0100

    build

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 THIS="${PWD/$GOPATH/}"
-
-echo ${THIS}
-docker run -ti --rm  -v ${PWD}:/go${THIS} kildevaeld/go-builder sh 
+MOUNT="/go${THIS}"
+docker run -ti --rm  -v ${PWD}:${MOUNT} -w ${MOUNT} kildevaeld/go-builder sh 
